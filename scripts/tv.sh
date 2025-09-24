57caa67b68b5926f0f5ae0bb9c38c5a9657eeec0
luoluodaduan <63093569+luoluodaduan@users.noreply.github.com>
2025-09-25 05:56:35 +0800

    fix

--- scripts/tv.sh
+++ scripts/tv.sh
@@ -2,35 +2,33 @@
 
 rm -rf fan/[0-9].json
 
-curl -o- "https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/lem.json" | sed 's/\r/\n/g' | grep -vE '^(\s*)(//|"notice"|"logo")' | grep -v "^$" >dist/1.json
-curl -o- "https://raw.githubusercontent.com/xyq254245/xyqonlinerule/main/XYQTVBox.json" | sed 's/\r/\n/g' | grep -vE '^(\s*)(//|"notice"|"logo")' | grep -v "^$" >dist/2.json
-curl -o- "https://raw.githubusercontent.com/yoursmile66/TVBox/main/XC.json" | sed 's/\r/\n/g' | grep -vE '^(\s*)(//|"notice"|"logo")' | grep -v "^$" >dist/3.json
-curl -o- "https://raw.githubusercontent.com/ne7359/tvurl/main/0821.json" | sed 's/\r/\n/g' | grep -vE '^(\s*)(//|"notice"|"logo")' | grep -v "^$" >dist/4.json
-curl -o- "https://raw.githubusercontent.com/ne7359/tvurl/main/xiaosa/api.json" | sed 's/\r/\n/g' | grep -vE '^(\s*)(//|"notice"|"logo")' | grep -v "^$" >dist/5.json
-curl -o- "https://raw.githubusercontent.com/wanganni/yinshiyuan/main/tvbox18.json" | sed 's/\r/\n/g' | grep -vE '^(\s*)(//|"notice"|"logo")' | grep -v "^$" >dist/6.json
+curl -o- --connect-timeout 10 --retry 3 "https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/lem.json" | sed 's/\r/\n/g' | grep -vE '^(\s*)(//|"notice"|"logo")' | grep -v "^$" >dist/1.json
+curl -o- --connect-timeout 10 --retry 3 "https://raw.githubusercontent.com/xyq254245/xyqonlinerule/main/XYQTVBox.json" | sed 's/\r/\n/g' | grep -vE '^(\s*)(//|"notice"|"logo")' | grep -v "^$" >dist/2.json
+curl -o- --connect-timeout 10 --retry 3 "https://raw.githubusercontent.com/yoursmile66/TVBox/main/XC.json" | sed 's/\r/\n/g' | grep -vE '^(\s*)(//|"notice"|"logo")' | grep -v "^$" >dist/3.json
+curl -o- --connect-timeout 10 --retry 3 "https://raw.githubusercontent.com/ne7359/tvurl/main/0821.json" | sed 's/\r/\n/g' | grep -vE '^(\s*)(//|"notice"|"logo")' | grep -v "^$" >dist/4.json
+curl -o- --connect-timeout 10 --retry 3 "https://raw.githubusercontent.com/ne7359/tvurl/main/xiaosa/api.json" | sed 's/\r/\n/g' | grep -vE '^(\s*)(//|"notice"|"logo")' | grep -v "^$" >dist/5.json
+curl -o- --connect-timeout 10 --retry 3 "https://raw.githubusercontent.com/wanganni/yinshiyuan/main/tvbox18.json" | sed 's/\r/\n/g' | grep -vE '^(\s*)(//|"notice"|"logo")' | grep -v "^$" >dist/6.json
 find dist -name '6.json' | xargs perl -pi -e "s/\'/\"/g"
 
 find dist -name '[0-9].json' | xargs perl -pi -e 's|\t| |g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's|"http([s]?)://([^"]+)(?<!:)\/\/([^"]*)"|"http$1://$2/$3"|g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's|"http([s]?)://([^"]+)(?<!:)\/\/([^"]*)"|"http$1://$2/$3"|g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's|http([s]?):(/{1,4})([^/]+)|http$1://$3|g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's|"http([s]?)://([^"]+)(?<!:)//([^"]*)"|"http$1://$2/$3"|g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's|"http([s]?)://([^"]+)(?<!:)//([^"]*)"|"http$1://$2/$3"|g'
 find dist -name '[0-9].json' | xargs perl -pi -e 's|(["=])(\.{2,3})/|$1./|g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's|"\./([^"]+)(?<!:)\/\/([^"]*)"|"./$1/$2"|g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's|"\./([^"]+)(?<!:)\/\/([^"]*)"|"./$1/$2"|g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's|(?<!:)\/\/.*||g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's|^(\s*)\/\*|『|g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's|\*\/(\s*)$|』|g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's|"\./([^"]+)(?<!:)//([^"]*)"|"./$1/$2"|g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's|"\./([^"]+)(?<!:)//([^"]*)"|"./$1/$2"|g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's|(?<!:)//.*||g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's|/refs/heads/|/|g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's|^(\s*)/\*|『|g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's|\*/(\s*)$|』|g'
 find dist -name '[0-9].json' | xargs perl -pi -e 's|\n||g'
 find dist -name '[0-9].json' | xargs perl -pi -e 's|『[\s\S]*』||g'
 find dist -name '[0-9].json' | xargs perl -pi -e 's/("lives"|"doh")\s*:\s*\[((?!\],).)+(\],)//g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's/"name"\s*:\s*"\s*([^" ]+)\s*(┃|｜|\|)\s*([^" ]+)\s*"/"name":"$1$3"/g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's/"name"\s*:\s*"\s*([^" ]+)(\s+)([^" ]+)\s*"/"name":"$1$3"/g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's/(#短剧\$([0-9]+)|"短剧",|"短剧大全",|"音乐",|"预告片",|"汽车",|"搞笑",|"影视",)//g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's/\{([^\{]*)"key"((?!"key").)+(Notice|Market|FirstAid|csp_Push|LocalFile|YGPGuard|YouTube|Youtube|bili|Bili|DJMV)((?!"key").)+\{([^\{]*)"key"/{$5"key"/g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's/\{([^\{]*)"key"((?!"key").)+(哔哩|短剧|少儿|戏曲|墙外|小说|童趣|兔小贝|听书|广播|相声|评书|音乐|短视频|酷奇MV)((?!"key").)+\{([^\{]*)"key"/{$5"key"/g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's/\{([^\{]*)"key"((?!"key").)+(Notice|Market|FirstAid|csp_Push|LocalFile|YGPGuard|YouTube|Youtube|bili|Bili|DJMV)((?!"key").)+\{([^\{]*)"key"/{$5"key"/g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's/\{([^\{]*)"key"((?!"key").)+(哔哩|短剧|少儿|戏曲|墙外|小说|童趣|兔小贝|听书|广播|相声|评书|音乐|短视频|酷奇MV)((?!"key").)+\{([^\{]*)"key"/{$5"key"/g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's/\{([^\{]*)"key"((?!"key").)+(Notice|Market|FirstAid|csp_Push|LocalFile|YGPGuard|YouTube|Youtube|bili|Bili|DJMV)((?!"key").)+\{([^\{]*)"key"/{$5"key"/g'
-find dist -name '[0-9].json' | xargs perl -pi -e 's/\{([^\{]*)"key"((?!"key").)+(哔哩|短剧|少儿|戏曲|墙外|小说|童趣|兔小贝|听书|广播|相声|评书|音乐|短视频|酷奇MV)((?!"key").)+\{([^\{]*)"key"/{$5"key"/g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's/\{([^\{]*)"key"((?!"key").)+(公告|说明|Market|FirstAid|csp_Push|LocalFile|YouTube|Youtube|bili|Bili|DJMV|哔哩|儿童|戏曲|墙外|小说|童趣|兔小贝|听书|广播|相声|评书|酷奇MV|翻墙|任何广告|push_agent|Cloud-drive|alitoken|csp_Pan|AList|配置|peizhi|网盘|云盘)((?!"key").)+\{([^\{]*)"key"/{$5"key"/g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's/\{([^\{]*)"key"((?!"key").)+(公告|说明|Market|FirstAid|csp_Push|LocalFile|YouTube|Youtube|bili|Bili|DJMV|哔哩|儿童|戏曲|墙外|小说|童趣|兔小贝|听书|广播|相声|评书|酷奇MV|翻墙|任何广告|push_agent|Cloud-drive|alitoken|csp_Pan|AList|配置|peizhi|网盘|云盘)((?!"key").)+\{([^\{]*)"key"/{$5"key"/g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's/\{([^\{]*)"key"((?!"key").)+(公告|说明|Market|FirstAid|csp_Push|LocalFile|YouTube|Youtube|bili|Bili|DJMV|哔哩|儿童|戏曲|墙外|小说|童趣|兔小贝|听书|广播|相声|评书|酷奇MV|翻墙|任何广告|push_agent|Cloud-drive|alitoken|csp_Pan|AList|配置|peizhi|网盘|云盘)((?!"key").)+\{([^\{]*)"key"/{$5"key"/g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's/\{([^\{]*)"key"((?!"key").)+(公告|说明|Market|FirstAid|csp_Push|LocalFile|YouTube|Youtube|bili|Bili|DJMV|哔哩|儿童|戏曲|墙外|小说|童趣|兔小贝|听书|广播|相声|评书|酷奇MV|翻墙|任何广告|push_agent|Cloud-drive|alitoken|csp_Pan|AList|配置|peizhi|网盘|云盘)((?!"key").)+\{([^\{]*)"key"/{$5"key"/g'
+find dist -name '[0-9].json' | xargs perl -pi -e 's/,\s*\{([^\{]*)"key"((?!("key"|\],)).)+(公告|说明|Market|FirstAid|csp_Push|LocalFile|YouTube|Youtube|bili|Bili|DJMV|哔哩|儿童|戏曲|墙外|小说|童趣|兔小贝|听书|广播|相声|评书|酷奇MV|翻墙|任何广告|push_agent|Cloud-drive|alitoken|csp_Pan|AList|配置|peizhi|网盘|云盘)((?!("key"|\],)).)+([^\[]+)("parses"|"rules")/\],$8/g'
 
 find dist -name '1.json' | xargs jq '.' >fan/1.json
 find dist -name '2.json' | xargs jq '.' >fan/2.json
@@ -39,8 +37,13 @@ find dist -name '4.json' | xargs jq '.' >fan/4.json
 find dist -name '5.json' | xargs jq '.' >fan/5.json
 find dist -name '6.json' | xargs jq '.' >fan/6.json
 
-find fan -name '[0-9].json' | xargs perl -pi -e 's|/refs/heads/|/|g'
-find fan -name '[0-9].json' | xargs perl -pi -e 's|(["=])https?:/([^"=]*)/raw\.([^/]+)/([^/]+)/|$1https://gh-proxy.com/https://raw.githubusercontent.com/$4/|g'
+find fan -name '[0-9].json' | xargs perl -pi -e 's/"name": "([^" ]*)\s*([^" ]*)\s*([^" ]*)\s*([^" ]*)\s*([^" ]*)"/"name": "$1$2$3$4$5"/g'
+find fan -name '[0-9].json' | xargs perl -pi -e 's/"name": "([^" ]*)(\||｜|┃|\[密\]|雷蒙影视|4K弹幕|\[无水印\])([^" ]*)"/"name": "$1$3"/g'
+find fan -name '[0-9].json' | xargs perl -pi -e 's/"name": "([^" ]*)(\||｜|┃|\[密\]|雷蒙影视|4K弹幕|\[无水印\])([^" ]*)"/"name": "$1$3"/g'
+find fan -name '[0-9].json' | xargs perl -pi -e 's/"name": "([^" ]*)(（|\[)([^" ]*)"/"name": "$1($3"/g'
+find fan -name '[0-9].json' | xargs perl -pi -e 's/"name": "([^" ]*)(）|\])([^" ]*)"/"name": "$1)$3"/g'
+
+find fan -name '[0-9].json' | xargs perl -pi -e 's|(["=])https?:/([^"=]*)/raw\.([^/]+)/([^/]+)/([^/]+)/|$1https://gh-proxy.com/https://raw.githubusercontent.com/$4/$5/|g'
 find fan -name '[0-9].json' | xargs perl -pi -e 's|(["=])https?:/([^"=]*)/github\.com/([^/]+)/([^/]+)/raw/|$1https://gh-proxy.com/https://raw.githubusercontent.com/$3/$4/|g'
 find fan -name '[0-9].json' | xargs perl -pi -e 's|(["=])https?:/([^"=]+)\.jsdelivr\.net/gh/([^/]+)/([^/]+)@|$1https://gh-proxy.com/https://raw.githubusercontent.com/$3/$4/|g'
 
@@ -50,5 +53,3 @@ find fan -name '3.json' | xargs perl -pi -e 's|(["=])\./|$1https://gh-proxy.com/
 find fan -name '4.json' | xargs perl -pi -e 's|(["=])\./|$1https://gh-proxy.com/https://raw.githubusercontent.com/ne7359/tvurl/main/|g'
 find fan -name '5.json' | xargs perl -pi -e 's|(["=])\./|$1https://gh-proxy.com/https://raw.githubusercontent.com/ne7359/tvurl/main/xiaosa/|g'
 find fan -name '6.json' | xargs perl -pi -e 's|(["=])\./|$1https://gh-proxy.com/https://raw.githubusercontent.com/wanganni/yinshiyuan/main/|g'
-
-find fan -name '3.json' | xargs perl -pi -e 's|"https://jihulab\.com/([^/]+)/([^/]+)/-/raw/|"https://gh-proxy.com/https://raw.githubusercontent.com/$1/$2/|g'
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sort -uf dist/apple1.txt | grep -v "^$" >gen/apple.txt
-sed "s/'\./'+\./" dist/apple2.txt | sort -uf | grep -v "^$" | sed -e '1s/^/payload:\n/' >coo/apple.yaml
+sed -r "s/'\./'+\./" dist/apple2.txt | sort -uf | grep -v "^$" | sed -e '1s/^/payload:\n/' >coo/apple.yaml
 
 sort -uf dist/cnip1.txt | grep -v "^$" >gen/cnip.txt
 sort -uf dist/cnip2.txt | grep -v "^$" | sed -e '1s/^/payload:\n/' >coo/cnip.yaml
@@ -9,15 +9,15 @@ sort -uf dist/cnip2.txt | grep -v "^$" | sed -e '1s/^/payload:\n/' >coo/cnip.yam
 grep -vE 'blogspot|discord|facebook|flickr|google|instagram|netflix|pinterest|porn|telegram|twitch|twitter|whatsapp|wikileaks|wikipedia|yahoo|youtube' dist/direct1.txt >dist/direct11.txt
 sort -uf dist/direct11.txt | grep -vE '[a-z0-9]\.cn$' | grep -v "^$" >gen/direct.txt
 grep -vE 'blogspot|discord|facebook|flickr|google|instagram|netflix|pinterest|porn|telegram|twitch|twitter|whatsapp|wikileaks|wikipedia|yahoo|youtube' dist/direct2.txt >dist/direct22.txt
-sed "s/'\./'+\./" dist/direct22.txt | grep -vE "[a-z0-9]\.cn'$" | sort -uf | grep -v "^$" | sed -e '1s/^/payload:\n/' >coo/direct.yaml
+sed -r "s/'\./'+\./" dist/direct22.txt | grep -vE "[a-z0-9]\.cn'$" | sort -uf | grep -v "^$" | sed -e '1s/^/payload:\n/' >coo/direct.yaml
 cat dist/direct3.txt | grep -vE 'blogspot|discord|facebook|flickr|google|instagram|netflix|pinterest|porn|telegram|twitch|twitter|whatsapp|wikileaks|wikipedia|yahoo|youtube' >dist/direct34.txt
-sed 's/server=\/\./server=\//' dist/direct34.txt | grep -vE "[a-z0-9]\.cn\/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$" | sort -uf | grep -v "^$" >fan/dnsmasqCN.txt
+sed 's|server=/\.|server=/|' dist/direct34.txt | grep -vE "[a-z0-9]\.cn/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$" | sort -uf | grep -v "^$" >fan/dnsmasqCN.txt
 sed 's/^\.//' dist/direct11.txt | grep -vE '[a-z0-9]\.cn$|jsdelivr\.|ghproxy\.|gh-proxy\.|moeyy\.xyz|1888866\.xyz|isteed\.cc' | sort -uf | grep -v "^$" >fan/smartdnsCN.txt
 
 sort -uf dist/proxy1.txt | grep -v "^$" >gen/proxy.txt
 sort -uf dist/proxy2.txt | grep -v "^$" | sed -e '1s/^/payload:\n/' >coo/proxy.yaml
 
-sed "s/[[:space:]]\+/ /g" dist/adguard1.txt | sed "s/^ \| $//g" | sort -u | grep -v "^$" >fan/adguard.txt
+sed -r "s/[[:space:]]+/ /g" dist/adguard1.txt | sed -r 's/^\s+|\s+$//g' | sort -u | grep -v "^$" >fan/adguard.txt
 grep -vE 'adservice|advertising|guanggao' dist/reject1.txt | sort -u | grep -v "^$" >gen/reject.txt
 grep -vE 'adservice|advertising|guanggao' dist/reject2.txt | sort -u | grep -v "^$" | sed -e '1s/^/payload:\n/' >coo/reject.yaml
 sort -uf dist/reject3.txt | grep -v "^$" >fan/dnsmasqAd.txt
